88c7eaeb11bfd5d7175ba0cc79542b8f65d33138
banahaker <pn0818x@outlook.com>
2024-02-28 23:53:29 +0800

    feat: styled ResultCard in find page

--- components/Find/ResultCard.tsx
+++ components/Find/ResultCard.tsx
@@ -1,14 +1,23 @@
+import { Button } from "@nextui-org/react";
 import { useSearchParams } from "next/navigation";
+import { FaCircleCheck } from "react-icons/fa6";
+import { IconContext } from "react-icons";
 
 function ResultCard() {
   const searchParams = useSearchParams();
   const domain = searchParams.get("sub");
   if (domain) {
     return (
-      <div className="w-full border border-neutral-600 rounded-lg px-3 flex flex-col items-start">
-        <p className="text-2xl">
-          {domain} <span>.dev-in.tw</span>
-        </p>
+      <div className="w-full border border-neutral-600 rounded-lg p-3 flex  items-center justify-between">
+        <div className="flex items-center gap-3">
+          <IconContext.Provider value={{ color: "green" }}>
+            <FaCircleCheck />
+          </IconContext.Provider>
+          <p className="text-2xl">
+            {domain} <span className=" text-default-400">.dev-in.tw</span>
+          </p>
+        </div>
+        <Button>申請</Button>
       </div>
     );
   } else {
