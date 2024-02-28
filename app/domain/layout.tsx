26e8da03ff229e3ef9e61829632e7014e1001219
HACO <haco.tw@gmail.com>
2024-02-28 14:44:24 +0800

    fix: Localize status labels in Domain/Status component

--- app/domain/layout.tsx
+++ app/domain/layout.tsx
@@ -3,7 +3,9 @@
 // icon
 import { AiOutlineGlobal } from "react-icons/ai";
 import { MdDns } from "react-icons/md";
+import { MdLock } from "react-icons/md";
 import { IoExtensionPuzzle } from "react-icons/io5";
+import { FaServer } from "react-icons/fa";
 
 // module
 import { Card } from "@nextui-org/react";
@@ -17,26 +19,33 @@ export default function DomainLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [actives, setActives] = useState(0);
-
   return (
     <div className="h-full select-none flex">
       <div className="w-full h-full py-6 my-auto max-h-[50rem]">
         <Card className="w-full h-full rounded-[1.5rem] flex flex-row">
           <div className="w-3/12 text-left rounded-xl p-4">
             <h1 className="text-white font-extrabold text-3xl text-center">DEV-IN.TW</h1>
-            <div className="flex flex-col gap-1 mt-5">
+            <div className="flex flex-col gap-2 mt-5">
               <button className={"text-center hover:bg-[#333333] py-2 px-2.5 rounded-md flex " + (PathCheck("/domain") && "bg-[#333333]")}>
                 <AiOutlineGlobal className="my-auto mr-2 w-6 h-6 text-[#5a5a5a]" />
                 我的子網域
               </button>
-              <button className={"text-left hover:bg-[#333333] py-2 px-2.5 rounded-md flex"}>
+              <button className={"text-left hover:bg-[#333333] py-2 px-2.5 rounded-md flex " + (PathCheck("/domain/dns") && "bg-[#333333]")}>
                 <MdDns className="my-auto mr-2 w-6 h-6 text-[#5a5a5a]" />
                 DNS 設定 (開發中)
               </button>
-              <button className={"text-center hover:bg-[#333333] py-2 px-2.5 rounded-md flex " + (PathCheck("/extension") && "bg-[#333333]")}>
+              <button className={"text-center hover:bg-[#333333] py-2 px-2.5 rounded-md flex " + (PathCheck("/domain/ssl") && "bg-[#333333]")}>
+                <MdLock className="my-auto mr-2 w-6 h-6 text-[#5a5a5a]" />
+                網站憑證 (開發中)
+              </button>
+              <button className={"text-center hover:bg-[#333333] py-2 px-2.5 rounded-md flex " + (PathCheck("/domain/extension") && "bg-[#333333]")}>
                 <IoExtensionPuzzle className="my-auto mr-2 w-6 h-6 text-[#5a5a5a]" />
                 擴充元件 (開發中)
+              </button>
+              <hr className="my-1 border-[#5a5a5a]" />
+              <button className={"text-center hover:bg-[#333333] py-2 px-2.5 rounded-md flex " + (PathCheck("/domain/server") && "bg-[#333333]")}>
+                <FaServer className="my-auto mr-2 w-6 h-6 text-[#5a5a5a]" />
+                租用伺服器 (開發中)
               </button>
             </div>
           </div>
--- components/Domain/Status.tsx
+++ components/Domain/Status.tsx
@@ -1,23 +1,23 @@
 export default function Status(prop: any) {
   if (prop.status === "active") {
     return (
-      <span className="text-green-500">ACTIVE</span>
+      <span className="text-green-500">可使用</span>
     )
   } else if (prop.status === "inactive") {
     return (
-      <span className="text-red-500">INACTIVE</span>
+      <span className="text-orange-500">不可使用</span>
     )
   } else if (prop.status === "reject") {
     return (
-      <span className="text-red-500">REJECT</span>
+      <span className="text-red-500">已駁回</span>
     )
   } else if (prop.status === "pending") {
     return (
-      <span className="text-yellow-500">PENDING</span>
+      <span className="text-yellow-500">審核中</span>
     )
   } else {
     return (
-      <span className="text-gray-500">UNKNOWN</span>
+      <span className="text-gray-500">未知</span>
     )
   }
 }
