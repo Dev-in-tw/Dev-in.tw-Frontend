a32b7c3c7c214b366da9e6aa8665eee20c4fb510
Haco <haco.tw@gmail.com>
2024-03-01 09:01:56 +0800

    feat: Add user login functionality
    - Created a new feature to allow users to log in to the platform
    - Implemented the login functionality in the Navbar component
    - Added a spinner to show loading state during login
    - Displayed user profile dropdown menu when logged in
    - Added logout functionality and updated user data when logged out
    - Updated the useUserAccount hook to handle user authentication and retrieve user data from local storage
    - Added useEffect hook to check if the user is logged in and update the login state accordingly

--- components/Global/Navbar.tsx
+++ components/Global/Navbar.tsx
@@ -21,18 +21,20 @@ import {
   NavbarItem,
   NavbarMenuItem,
 } from "@nextui-org/navbar";
+import { Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Spinner } from "@nextui-org/react";
 
 // hook
 import { useUserAccount } from "@/hooks/useUserAccount";
 import { useEffect } from "react";
 
+
 export default function NavbarC() {
   const router = useRouter();
-  const { token, userData, isLoading } = useUserAccount();
+  const { token, userData, isLoading, isLogin } = useUserAccount();
 
   useEffect(() => {
-    console.log(token, userData, isLoading);
-  }, [token, userData, isLoading]);
+    console.log(token, userData, isLoading, isLogin);
+  }, [token, userData, isLoading, isLogin]);
 
   return (
     <NextUINavbar position="sticky">
@@ -103,15 +105,53 @@ export default function NavbarC() {
         </Link>
         <ThemeSwitch />
         <NavbarItem className="md:hidden flex">
-          <Button
-            className="text-[1rem] rounded-full dark:text-[#eeeeee] dark:bg-[#292a2d] text-[#333333] bg-[#dddddd]"
-            variant="flat"
-            onClick={() => router.replace("/login")}
-          >
-            登入
-          </Button>
+          {
+            isLoading ?
+              (
+                <Spinner color="default" size="lg" />
+              ) : (
+                isLogin ? (
+                  <Dropdown placement="bottom-end">
+                    <DropdownTrigger>
+                      <Avatar
+                        isBordered
+                        as="button"
+                        className="transition-transform"
+                        color="secondary"
+                        name="Jason Hughes"
+                        size="sm"
+                        src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                      />
+                    </DropdownTrigger>
+                    <DropdownMenu aria-label="Profile Actions" variant="flat">
+                      <DropdownItem key="profile" className="h-14 gap-2">
+                        <p className="font-semibold">Signed in as</p>
+                        <p className="font-semibold">zoey@example.com</p>
+                      </DropdownItem>
+                      <DropdownItem key="settings">My Settings</DropdownItem>
+                      <DropdownItem key="team_settings">Team Settings</DropdownItem>
+                      <DropdownItem key="analytics">Analytics</DropdownItem>
+                      <DropdownItem key="system">System</DropdownItem>
+                      <DropdownItem key="configurations">Configurations</DropdownItem>
+                      <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+                      <DropdownItem key="logout" color="danger">
+                        Log Out
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </Dropdown>
+                ) : (
+                  <Button
+                    className="text-[1rem] rounded-full dark:text-[#eeeeee] dark:bg-[#292a2d] text-[#333333] bg-[#dddddd]"
+                    variant="flat"
+                    onClick={() => router.replace("/login")}
+                  >
+                    登入
+                  </Button>
+                )
+              )
+          }
         </NavbarItem>
       </NavbarContent>
-    </NextUINavbar>
+    </NextUINavbar >
   );
 }
--- hooks/useUserAccount/index.tsx
+++ hooks/useUserAccount/index.tsx
@@ -16,87 +16,24 @@ export function useUserAccount() {
 
   useEffect(() => {
     (async () => {
-      const token = localStorage.getItem("token");
+      const _token = localStorage.getItem("token");
 
-      if (token) {
-        setUserData(await apiClient.user.info.get(JSON.parse(token)));
+      if (_token) {
+        setUserData(await apiClient.user.info.get(JSON.parse(_token)));
+        setToken(JSON.parse(_token));
       }
-      console.log(userData, token, isLoading)
+
       setIsLoading(false);
     })();
   }, []);
 
-  return { token, userData, isLoading };
+  useEffect(() => {
+    (async () => {
+      if (token && userData) {
+        setIsLogin(true);
+      }
+    })();
+  }, [token, userData, isLoading]);
 
-  // useEffect(() => {
-  //   (async () => {
-  //     const userLocalStorage = localStorage.getItem("user");
-
-  //     if (OAuth === "loading" || !userLocalStorage) setIsLoading(true);
-
-  //     if (OAuth === "authenticated") {
-  //       try {
-  //         const accessToken = (session as any)?.accessToken;
-  //         const { token, data } = await platformLogin(accessToken);
-  //         localStorage.setItem("token", token);
-  //         localStorage.setItem("user", JSON.stringify(data));
-  //         setToken(token);
-  //         setUserData(data);
-  //         setIsLogin(true);
-  //         setIsLoading(false);
-  //       } catch (error) {
-  //         console.error(error);
-  //         setIsLogin(false);
-  //         signOut();
-  //       }
-  //     } else if (OAuth !== "loading") {
-  //       setIsLogin(false);
-  //       setIsLoading(false);
-  //     }
-  //   })();
-  // }, [OAuth, session]);
-
-  // useEffect(() => {
-  //   if (!isLogin) {
-  //     localStorage.removeItem("token");
-  //     localStorage.removeItem("user");
-  //     setToken(null);
-  //     setUserData(null);
-  //   }
-  // }, [isLogin]);
-
-  // const login = useCallback(() => {
-  //   // signIn("google");
-  // }, []);
-
-  // const logout = useCallback(() => {
-  //   signOut();
-  // }, []);
-
-  //   return { isLogin, token, userData, isLoading, login, logout };
-  // }
-
-  // async function platformLogin(accessToken: string) {
-  //   try {
-  //     const urlencoded = new URLSearchParams();
-  //     urlencoded.append("googleToken", accessToken);
-  //     const response = await (
-  //       await fetch(`${API_URL}/login`, {
-  //         method: "POST",
-  //         headers: {
-  //           "Content-Type": "application/x-www-form-urlencoded",
-  //         },
-  //         body: urlencoded,
-  //       })
-  //     ).json();
-
-  //     if (response.status !== 2000) throw new Error("Failed to login to platform");
-
-  //     return {
-  //       token: response.authorization.toString().split(" ")[1],
-  //       data: response.data,
-  //     };
-  //   } catch (error: any) {
-  //     throw new Error(error.message);
-  //   }
+  return { token, userData, isLoading, isLogin };
 }
